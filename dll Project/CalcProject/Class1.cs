ade52634b55bf045e15a457bb0c49f1f071725f3
SARANG MK <35254554+sarangsmk@users.noreply.github.com>
2021-12-18 13:08:54 +0530

    Added C# Dll Project

---
+++ dll Project/CalcProject/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CalcProject
+{
+    public class calculate
+    {
+        /// <summary>
+        /// Simple Helper to add 2 Numbers and return the result
+        /// </summary>
+        /// <param name="a">Number 1</param>
+        /// <param name="b">Number 2</param>
+        /// <returns>Sum of 2 Numbers</returns>
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
